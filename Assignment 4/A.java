bc462e69be65444cd9850ca6f9fd1cb1c6b45294
Atul5486 <atulkoushal5486@gmail.com>
2025-08-18 09:44:35 +0530

    Update A pattern

--- Assignment 4/A.java
+++ Assignment 4/A.java
@@ -1,26 +1,13 @@
-import java.util.Scanner;
 class Main{
     public static void main(String args[]){
-        Scanner inp=new Scanner(System.in);
-        System.out.print("Enter a number : ");
-        int num=inp.nextInt();
-        int temp=num;
-        int count=0;
-        int sum=0;
-        while(temp!=0){
-            temp/=10;
-            count++;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if(j==1 || j==5 || i==1 || i==3)
+                System.out.print("*");
+                else
+                System.out.print(" ");
+            }
+            System.out.println();
         }
-        temp=num;
-        while(temp!=0){
-            int rem=temp%10;
-            sum+=Math.pow(rem,count);
-            temp/=10;
-        }
-        if(sum==num)
-        System.out.println("Number is armstrong");
-        else
-        System.out.println("Number is not armstrong");
-
     }
 }
